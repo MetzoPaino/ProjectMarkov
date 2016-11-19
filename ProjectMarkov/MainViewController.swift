dbcbb776a3bb21beb760427ac993ac66ff11241f
William Robinson <williamjrobinson@me.com>
2016-11-19 16:36:12 +0000

    Created an action extension for capturing text

---
+++ ProjectMarkov/MainViewController.swift
@@ -0,0 +1,67 @@
+//
+//  MainViewController.swift
+//  ProjectMarkov
+//
+//  Created by William Robinson on 19/11/2016.
+//  Copyright © 2016 William Robinson. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    var localTextArray = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let defaults = UserDefaults(suiteName: "group.extensiontexttaker")
+        defaults?.synchronize()
+        
+        if let textArray = defaults!.object(forKey: "TextArray") {
+            
+            localTextArray = textArray as! [String]
+            
+        } else {
+            
+            let textArray = [String]()
+            defaults?.set(textArray, forKey: "TextArray")
+            defaults?.synchronize()
+        }
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension MainViewController: UITableViewDataSource {
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        cell.textLabel!.text = localTextArray[indexPath.row]
+        return cell
+        
+    }
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return localTextArray.count
+    }
+}
+
+extension MainViewController: UITableViewDelegate {
+    
+    
+}
+
---
+++ actionExtension/ActionViewController.swift
@@ -0,0 +1,71 @@
+//
+//  ActionViewController.swift
+//  actionExtension
+//
+//  Created by William Robinson on 19/11/2016.
+//  Copyright © 2016 William Robinson. All rights reserved.
+//
+
+import UIKit
+import MobileCoreServices
+
+class ActionViewController: UIViewController {
+
+    @IBOutlet weak var textView: UITextView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        let textItem = self.extensionContext!.inputItems[0] as! NSExtensionItem
+        let textItemProvider = textItem.attachments![0] as! NSItemProvider
+        
+        if textItemProvider.hasItemConformingToTypeIdentifier(
+            kUTTypeText as String) {
+            textItemProvider.loadItem(forTypeIdentifier: kUTTypeText as String, options: nil, completionHandler: {
+                (result, error) in
+                                        
+                                        var convertedString = result as? String
+                                        
+                                        if convertedString != nil {
+                                            
+                                            
+                                            DispatchQueue.main.async {
+                                                print(convertedString!)
+                                                self.textView.text = convertedString
+
+                                            }
+                                        }
+            })
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func done() {
+        // Return any edited content to the host app.
+        // This template doesn't do anything, so we just echo the passed in items.
+        self.extensionContext!.completeRequest(returningItems: self.extensionContext!.inputItems, completionHandler: nil)
+    }
+
+    @IBAction func save() {
+        
+        let defaults = UserDefaults(suiteName: "group.extensiontexttaker")
+
+        if var textArray = defaults!.object(forKey: "TextArray") as? [String] {
+            
+            textArray.append(textView.text)
+            defaults?.set(textArray, forKey: "TextArray")
+            
+        } else {
+            
+            var textArray = [String]()
+            textArray.append(textView.text)
+            defaults?.set(textArray, forKey: "TextArray")
+        }
+        defaults?.synchronize()
+
+    }
+}
