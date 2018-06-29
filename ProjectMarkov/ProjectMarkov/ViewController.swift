af20724710e2e04d9af0db7511e00107d5ac4daf
William <williamjrobinson@me.com>
2018-06-29 11:39:34 +0100

    New project

---
+++ ProjectMarkov/ProjectMarkov/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProjectMarkov
+//
+//  Created by William Robinson on 29/06/2018.
+//  Copyright © 2018 William Robinson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ ProjectMarkov/ProjectMarkovTests/ProjectMarkovTests.swift
@@ -0,0 +1,36 @@
+//
+//  ProjectMarkovTests.swift
+//  ProjectMarkovTests
+//
+//  Created by William Robinson on 29/06/2018.
+//  Copyright © 2018 William Robinson. All rights reserved.
+//
+
+import XCTest
+@testable import ProjectMarkov
+
+class ProjectMarkovTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
